f9652356b7cee0abe4e24e105e08bce5968180ab
Alexander Morozov <alexander.morozov@shijigroup.com>
2021-04-28 10:31:35 +0300

    implement part 6 b

--- part_6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part_6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotificationMessage } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -10,6 +11,8 @@ const AnecdoteForm = () => {
     const content = e.target.anecdote.value
     e.target.anecdote.value = ''
     dispatch(createAnecdote(content))
+    dispatch(setNotificationMessage(`anecdote '${content}' created`))
+    setTimeout(() => dispatch(setNotificationMessage('')) , 5000)
   }
 
   return (
--- part_6/redux-anecdotes/src/components/AnecdoteList.js
+++ part_6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
+import { setNotificationMessage } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+  const filter = useSelector(state => state.filter)
+  const anecdotes = useSelector(state => 
+    state.anecdotes
+      .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+      .sort((a, b) => b.votes - a.votes))
+
   const dispatch = useDispatch()
 
   const handleVote = (id) => {
+    const votedAnecdote = anecdotes.find(a => a.id === id) 
     dispatch(vote(id))
-    console.log('vote', id)
+    dispatch(setNotificationMessage(`you voted '${votedAnecdote.content}'`))
+    setTimeout(() => dispatch(setNotificationMessage('')) , 5000)
   }
 
   return (
