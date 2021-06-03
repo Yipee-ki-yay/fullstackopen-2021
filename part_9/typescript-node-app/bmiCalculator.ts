e06d907d073885a19025dcc9e1f94e8bdcedb3ff
Alexander Morozov <alexander.morozov@shijigroup.com>
2021-06-03 16:12:20 +0300

    implement ex 9.7

--- part_9/typescript-node-app/bmiCalculator.ts
+++ part_9/typescript-node-app/bmiCalculator.ts
@@ -1,19 +1,25 @@
-const height: number = Number(process.argv[2])
-const weight: number = Number(process.argv[3])
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
 
-const calculateBmi = (height: number, weight: number): void => {
-  const result = weight / ((height / 100) * (height / 100))
+export const calculateBmi = (height: number, weight: number): string => {
+  const result = weight / ((height / 100) * (height / 100));
 
   if (result < 18.5) {
-    console.log(`Underweight (${height} ${weight})`)
+    console.log(`Underweight (${height} ${weight})`);
+    return `Underweight (${height} ${weight})`;
   } else if (result >= 18.5 && result < 25) {
-    console.log(`Normal (${height} ${weight})`)
+    console.log(`Normal (${height} ${weight})`);
+    return `Normal (${height} ${weight})`;
   } else if (result >= 25 && result < 30) {
-    console.log(`Overweight (${height} ${weight})`)
+    console.log(`Overweight (${height} ${weight})`);
+    return `Overweight (${height} ${weight})`;
   } else if (result >= 30) {
-    console.log(`Obese (${height} ${weight})`)
+    console.log(`Obese (${height} ${weight})`);
+    return `Obese (${height} ${weight})`;
   }
-}
 
-// console.log(calculateBmi(180, 74))
-console.log(calculateBmi(height, weight))
+  return '';
+};
+
+// console.log(calculateBmi(180, 74));
+console.log(calculateBmi(height, weight));
