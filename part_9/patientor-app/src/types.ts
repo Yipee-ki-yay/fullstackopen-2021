be87080571f36dca5978aeeae6b3ef7f50817115
Alexander Morozov <alexander.morozov@shijigroup.com>
2021-06-17 15:40:41 +0300

    implement ex 9.22

--- part_9/patientor-app/src/types.ts
+++ part_9/patientor-app/src/types.ts
@@ -14,10 +14,56 @@ export interface Diagnose {
   latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
 }
 
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthCareEntry
+  | HealthCheckEntry;
+
+// Define special omit for unions
+// type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+// type EntryWithoutId = UnionOmit<Entry, 'id'>;
 export interface Patient {
   id: string | number;
   name: string;
